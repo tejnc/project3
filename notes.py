bf9ac8b44298b0039d2ec22f95ddded9d68f3622
tejnc <tejnarayan15.tnc@gmail.com>
2021-10-26 20:43:19 +0545

    beginning

---
+++ notes.py
@@ -0,0 +1,19 @@
+"""
+    attritubes: 
+        title, description, likes, comments: [{by user, date}], status, created_at, updated_at, notes_by
+"""
+notes = {
+    "title":" ",
+    "description" : " ",
+    "likes" : " ",
+    "comments" : {
+        "by_user":" ",
+        "date":" "
+    },
+    "status" : " ",
+    "created_at" : " ",
+    "updated_at" : " ",
+    "notes_by" : " ",
+}
+
+
